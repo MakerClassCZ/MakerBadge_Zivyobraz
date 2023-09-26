b3518b0768d4dfc5763ffedddb2ae8380e3e491b
Vladimír Smitka <vladimir.smitka@lynt.cz>
2023-09-26 21:05:23 +0200

    Initial

---
+++ code.py
@@ -0,0 +1,104 @@
+
+import displayio
+import wifi
+import socketpool
+import adafruit_requests
+import os
+import time
+from mb_setup import setup, get_battery
+import alarm
+import microcontroller
+
+(display, touch, led_matrix, colors) = setup()
+
+# Connect to WiFi
+wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_PASSWORD'))
+
+print("Conencted:", wifi.radio.ap_info.ssid)
+
+rssi = wifi.radio.ap_info.rssi
+mac_addr = ":".join([hex(i).replace("0x","").upper() for i in wifi.radio.mac_address])
+
+battery_voltage = get_battery()
+print("Battery voltage: ", battery_voltage)
+
+URL = f"http://cdn.zivyobraz.eu/index.php?mac={mac_addr}&timestamp_check=1&rssi={rssi}&v={battery_voltage}&x=250&y=122&c=BW&fw=1"
+
+def parse_bmp(bmp_data):
+
+    if bmp_data[:2] != b'BM':
+        print(bmp_data[:50])
+        return displayio.Bitmap(1, 1, 1)
+
+    image_offset = int.from_bytes(bmp_data[10:14], 'little')
+    
+    width = int.from_bytes(bmp_data[18:22], 'little')
+    height = int.from_bytes(bmp_data[22:26], 'little')
+    
+    padding = (4 - ((width // 8) % 4)) % 4
+    
+    image_bitmap = displayio.Bitmap(width, height, 2)
+
+    data_pointer = image_offset
+    for y in range(height - 1, -1, -1):  # Start from the bottom row
+        for x in range(width // 8):  # 1 byte = 8 pixels
+            if data_pointer < len(bmp_data):
+                byte = bmp_data[data_pointer]
+                
+                mask = 0b10000000
+                for bit in range(8):
+                    j = y * width + x * 8 + bit
+                    if j < width * height:
+                        image_bitmap[j] = (byte & mask) >> (7 - bit)
+                    mask >>= 1
+
+                data_pointer += 1
+        data_pointer += padding
+
+    return image_bitmap
+
+  
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool)
+
+print("zivyobraz URL:", URL)
+resp = requests.get(URL)
+
+# Get headers
+sleep_time = int(resp.headers['sleep'])*60
+timestamp = int(resp.headers['timestamp'])
+
+# Get previous timestamp from NVM
+bytes_timestamp = microcontroller.nvm[0:4]
+timestamp_old = int.from_bytes(bytes_timestamp, 'little')
+
+print(sleep_time, timestamp, timestamp_old)
+
+if timestamp != timestamp_old:
+
+    print("New image available!")
+
+    bitmap = parse_bmp(resp.content)
+    palette = displayio.Palette(2)
+    palette[0] = 0x000000
+    palette[1] = 0xFFFFFF
+
+    tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+    group = displayio.Group()
+    group.append(tile_grid)
+    display.show(group)
+    display.refresh()
+
+    while display.busy:
+        pass
+
+    bytes_timestamp = timestamp.to_bytes(4, 'little')
+    microcontroller.nvm[0:4] = bytes_timestamp
+
+
+    
+time_alarm = alarm.time.TimeAlarm(monotonic_time=time.monotonic() + sleep_time)
+print(f"Good night for {sleep_time}s!")
+alarm.exit_and_deep_sleep_until_alarms(time_alarm)
+
+
---
+++ mb_setup.py
@@ -0,0 +1,104 @@
+import board
+import time
+import neopixel
+import touchio
+import displayio
+import adafruit_ssd1680
+from digitalio import DigitalInOut, Direction
+from adafruit_debouncer import Debouncer
+from adafruit_display_text import bitmap_label
+import adafruit_miniqr
+import analogio
+
+
+
+colors = {
+        'black':  0x000000,
+        'white':  0xFFFFFF,
+        'red':    0xFF0000,
+        'green':  0x00FF00,
+        'blue':   0x0000FF,
+        'yellow': 0xFFFF00,
+}
+
+def setup():
+
+    # Define board pinout
+    board_spi = board.SPI()  # Uses SCK and MOSI
+    board_epd_cs = board.D41
+    board_epd_dc = board.D40
+    board_epd_reset = board.D39
+    board_epd_busy = board.D42
+    enable_display = DigitalInOut(board.D16)
+    enable_display.direction = Direction.OUTPUT
+    enable_display.value = False
+
+
+
+
+    # Define touch buttons
+    touch_threshold = 20000
+    touch = []
+    for pin in [board.D5,board.D4,board.D3,board.D2,board.D1]:
+        tmp = touchio.TouchIn(pin)
+        tmp.threshold = touch_threshold
+        touch.append(Debouncer(tmp))
+
+    led_matrix = neopixel.NeoPixel(board.D18, 4, brightness = 0.1, auto_write = False)
+
+
+
+    # Define ePaper display resolution
+    display_width = 250
+    display_height = 122
+
+    # Prepare ePaper display
+    displayio.release_displays()
+    display_bus = displayio.FourWire(
+        board_spi, command = board_epd_dc, chip_select = board_epd_cs, reset = board_epd_reset, baudrate = 1000000
+    )
+
+
+    time.sleep(1)
+
+    display = adafruit_ssd1680.SSD1680(
+        display_bus, width = display_width, height = display_height, rotation = 270, busy_pin = board_epd_busy
+    )
+    
+
+    return (display, touch, led_matrix, colors)
+
+def get_battery():
+    vbat_voltage = analogio.AnalogIn(board.D6)
+    enable_battery_reading = DigitalInOut(board.D14)
+    enable_battery_reading.direction = Direction.OUTPUT
+    enable_battery_reading.value = False
+    bat_value = (vbat_voltage.value * 3.3) / 65536 * 2
+    enable_battery_reading.value = True
+    return bat_value
+
+def qr_gen(data):
+    qrcode = adafruit_miniqr.QRCode(qr_type=1)
+    qrcode.add_data(data)
+    qrcode.make()
+
+    # bitmap the size of the matrix, plus border, monochrome (2 colors)
+    qr_bitmap = displayio.Bitmap(qrcode.matrix.width + 2, qrcode.matrix.height + 2, 2)
+    for i in range(qr_bitmap.width * qr_bitmap.height):
+        qr_bitmap[i] = 0
+
+    # transcribe QR code into bitmap
+    for xx in range(qrcode.matrix.width):
+        for yy in range(qrcode.matrix.height):
+            qr_bitmap[xx + 1, yy + 1] = 1 if qrcode.matrix[xx, yy] else 0
+    
+    return qr_bitmap
+
+
+
+def text_gen(text, x, y, font, scale = 1, color = colors['black']):
+
+    label = bitmap_label.Label(font, text = text, color = color, scale = scale)
+    label.x = x
+    label.y = y
+    return label
