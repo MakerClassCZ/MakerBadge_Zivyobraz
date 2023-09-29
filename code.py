bd4c3808bb4c5583e5a8fcd458641defe8fd209d
Vladimír Smitka <vladimir.smitka@lynt.cz>
2023-09-29 08:56:47 +0200

    Better mac address parser

--- code.py
+++ code.py
@@ -17,7 +17,7 @@ wifi.radio.connect(os.getenv('CIRCUITPY_WIFI_SSID'), os.getenv('CIRCUITPY_WIFI_P
 print("Conencted:", wifi.radio.ap_info.ssid)
 
 rssi = wifi.radio.ap_info.rssi
-mac_addr = ":".join([hex(i).replace("0x","").upper() for i in wifi.radio.mac_address])
+mac_addr = ":".join([f"{i:02x}" for i in wifi.radio.mac_address])
 
 battery_voltage = get_battery()
 print("Battery voltage: ", battery_voltage)
